90de966ffee21c883a0eb74d444b608cc8e55000
Christopher Smith <ctsmith2308@gmail.com>
2021-09-06 17:13:15 -0600

    add hot reloading to webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,9 +4,11 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'development',
+  devtool: 'inline-source-map',
   devServer: {
     static: {
       directory: path.join(__dirname, 'src'),
+      hot: true,
     },
     compress: true,
   },
